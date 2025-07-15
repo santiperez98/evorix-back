be1e4350e8fe93fa0f9cd5b795d0bb52ce3551f7
santiperez98 <santip19982022@gmail.com>
2025-07-15 15:04:07 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // 👈 importante!
+    port: process.env.DB_PORT, 
     dialect: 'postgres',
     logging: false,
   }
